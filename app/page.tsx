f0e6cf57d352595a523148990aab3b7208d24c4a
Batur <halidbaturakkurt@gmail.com>
2025-02-09 15:28:15 +0300

    feat: slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Slider } from "@/components/ui";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen w-full p-8 pb-20 gap-16 sm:p-20">
@@ -5,6 +7,13 @@ export default function Home() {
         <h1 className="text-8xl font-bold text-center sm:text-left text-blue-400">
           Equilibrium
         </h1>
+        <section className="flex gap-8 justify-between w-full">
+          <Slider orientation="vertical" min={0} max={10} className="h-72" />
+          <Slider orientation="vertical" min={0} max={10} className="h-72" />
+          <Slider orientation="vertical" min={0} max={10} className="h-72" />
+          <Slider orientation="vertical" min={0} max={10} className="h-72" />
+          <Slider orientation="vertical" min={0} max={10} className="h-72" />
+        </section>
       </main>
     </div>
   );
---
+++ components/ui/slider.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const trackVariants = cva("relative grow overflow-hidden rounded-full", {
+  variants: {
+    orientation: {
+      vertical: "h-full w-1.5",
+      horizontal: "w-full h-1.5",
+    },
+    variant: {
+      primary: "bg-primary/20",
+      secondary: "bg-secondary/20",
+    },
+    defaultVariants: {
+      orientation: "horizontal",
+      variant: "primary",
+    },
+  },
+});
+const rangeVariants = cva("absolute", {
+  variants: {
+    orientation: {
+      vertical: "w-full",
+      horizontal: "h-full",
+    },
+    variant: {
+      primary: "bg-primary",
+      secondary: "bg-secondary",
+    },
+    defaultVariants: {
+      orientation: "horizontal",
+      variant: "primary",
+    },
+  },
+});
+
+interface SliderProps extends VariantProps<typeof trackVariants> {
+  orientation?: "vertical" | "horizontal";
+}
+
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  SliderProps & React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(
+  (
+    { className, orientation = "horizontal", variant = "primary", ...props },
+    ref
+  ) => (
+    <SliderPrimitive.Root
+      ref={ref}
+      orientation={orientation}
+      className={cn(
+        "relative flex touch-none select-none items-center",
+        orientation === "vertical" ? "flex-col h-full w-4" : "w-full h-4",
+        className
+      )}
+      {...props}
+    >
+      <SliderPrimitive.Track
+        className={cn(trackVariants({ orientation, variant }))}
+      >
+        <SliderPrimitive.Range
+          className={cn(rangeVariants({ orientation, variant }))}
+        />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+    </SliderPrimitive.Root>
+  )
+);
+Slider.displayName = SliderPrimitive.Root.displayName;
+
+export { Slider };
